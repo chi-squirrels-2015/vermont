9ea2e1b8a2927aa33185e3fedbe68ac67ae0953f
Edward Mitchell & John Schulz <pair+elm0829.nimylian@devbootcamp.com>
2015-02-09 16:59:03 -0600

    Begin displaying guns

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,17 +11,6 @@ fn = Manufacturer.create!(name: "FN")
 
 gunstore = Store.create!(name: "TheGunStore")
 
-# g1 = Gun.create!(model: "M4A1", price: 500.00, manufacturers_id: colt.id)
-# g2 = Gun.create!(model: "700", price: 400.00, manufacturers_id: remington.id)
-# g3 = Gun.create!(model: "Desert Eagle", price: 2_000.00, manufacturers_id: magnumResearch.id)
-# g4 = Gun.create!(model: "M107", price: 10_000.00, manufacturers_id: barrett.id)
-# g5 = Gun.create!(model: "M203", price: 1_000.00, manufacturers_id: colt.id)
-# g6 = Gun.create!(model: "F35B", price: 142_000_000.00, manufacturers_id: lockheedMartin.id)
-# g7 = Gun.create!(model: "AT4", price: 1_400.00, manufacturers_id: saab.id)
-# g8 = Gun.create!(model: "M249 SAW", price: 1_400.00, manufacturers_id: fn.id)
-
-
-
 20.times do
   Gun.create!(model: "M4A1", price: 500.00, manufacturer: colt, store: gunstore)
 end
@@ -35,7 +24,7 @@ end
 end
 
 15.times do
-  Gun.create!(model: "M4A1", price: 500.00, manufacturer: colt, store: gunstore)
+  Gun.create!(model: "M203", price: 500.00, manufacturer: colt, store: gunstore)
 end
 
 3.times do
